44a2de797917b839d3178d9ce758132b5561424c
YannickUllisch <yannicku01@gmail.com>
2024-09-12 15:56:07 +0200

    exercises week 2

--- week2/handout/src/APL/Eval.hs
+++ week2/handout/src/APL/Eval.hs
@@ -1,7 +1,10 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Eta reduce" #-}
 module APL.Eval
   ( Val (..),
-    eval,
     runEval,
+    eval,
+    envEmpty,
     Error,
   )
 where
@@ -24,14 +27,147 @@ envExtend :: VName -> Val -> Env -> Env
 envExtend v val env = (v, val) : env
 
 envLookup :: VName -> Env -> Maybe Val
-envLookup v env = lookup v env
+envLookup = lookup
 
 type Error = String
 
-newtype EvalM a = EvalM a -- TODO
+newtype EvalM a = EvalM (Env -> Either Error a)
+
+instance Functor EvalM where
+  fmap f (EvalM x) =
+    EvalM $ \env -> case x env of
+      Right v -> Right $ f v
+      Left err -> Left err
+
+
+instance Applicative EvalM where
+  pure x = EvalM $ \_ -> Right x
+  EvalM func <*> EvalM ax = EvalM $ \env ->
+    case (func env, ax env) of 
+      (Left err, _) -> Left err
+      (_, Left err) -> Left err
+      (Right f, Right x) -> Right $ f x
+
+
+instance Monad EvalM where
+  return = pure 
+  EvalM a >>= f = EvalM $ \env ->
+    case a env of 
+      Left err -> Left err
+      Right x' -> 
+        let EvalM y = f x'
+        in y env 
+
+
+askEnv :: EvalM Env
+askEnv = EvalM $ \env -> Right env
+
+localEnv :: (Env -> Env) -> EvalM a -> EvalM a
+localEnv f (EvalM a) = EvalM $ \env -> a (f env)
 
 runEval :: EvalM a -> Either Error a
-runEval = undefined -- TODO
+runEval (EvalM f) = f envEmpty
 
-eval :: Env -> Exp -> EvalM Val
-eval = undefined -- TODO
+failure :: String -> EvalM a
+failure s = EvalM $ \_ -> Left s 
+
+catch :: EvalM a -> EvalM a -> EvalM a
+catch (EvalM m1) (EvalM m2) = EvalM $ \env ->
+  case m1 env of 
+    Left _ -> m2 env
+    Right a -> Right a
+
+
+eval :: Exp -> EvalM Val
+eval (CstInt a) = pure $ ValInt a 
+eval (CstBool a) = pure $ ValBool a 
+eval (Var n) = do
+  env <- askEnv
+  case envLookup n env of
+    Just x -> EvalM $ \_ -> Right x
+    Nothing -> failure "Unknown Variable"
+
+-- Addition Case
+eval (Add a b) = do
+  x <- eval a 
+  y <- eval b
+  case (x, y) of 
+    (ValInt a', ValInt b') -> pure $ ValInt $ a' + b'
+    _ -> failure "Non-integer operand"
+
+-- Subtraction Case
+eval (Sub a b) = do
+  x <- eval a 
+  y <- eval b
+  case (x, y) of 
+    (ValInt a', ValInt b') -> pure $ ValInt $ a' - b'
+    _ -> failure "Non-integer operand"
+
+-- Multiplication Case
+eval (Mul a b) = do
+  x <- eval a 
+  y <- eval b
+  case (x, y) of 
+    (ValInt a', ValInt b') -> pure $ ValInt $ a' * b'
+    _ -> failure "Non-integer operand"
+
+-- Multiplication Case
+eval (Div a b) = do
+  x <- eval a 
+  y <- eval b
+  case (x, y) of 
+    (ValInt a, ValInt b) -> checkedDiv a b
+      where 
+        checkedDiv _ 0 = failure "Division by zero"
+        checkedDiv u v = pure $ ValInt $ u `div` v 
+    _ -> failure "Non-integer operand"
+
+-- Pow Case
+eval (Pow a b) = do
+  x <- eval a
+  y <- eval b
+  case (x,y) of
+    (ValInt a, ValInt b) -> checkedPow a b
+      where
+        checkedPow _ y' | y' < 0 = failure "Negative exponent"
+        checkedPow x' y' = pure $ ValInt $ x' ^ y'
+    _ -> failure "Non-integer operand"
+
+-- TryCatch case
+eval (TryCatch e1 e2) = catch (eval e1) (eval e2)
+
+-- Eql Case
+eval (Eql a b) = do
+  x <- eval a
+  y <- eval b
+  case (x,y) of
+    (ValInt x, ValInt y) -> pure $ ValBool $ x == y
+    (ValBool x, ValBool y) -> pure $ ValBool $ x == y
+    (_, _) -> failure "Invalid operands to equality"
+
+-- If Case
+eval (If cond a b) = do
+  cond' <- eval cond
+  case cond' of
+    ValBool True -> eval a
+    ValBool False -> eval b
+    _ -> failure "Invalid operands to equality"
+
+-- Let Case
+eval (Let var a b) = do
+  v1 <- eval a
+  localEnv (envExtend var v1) $ eval b
+
+-- Let Case
+eval (Lambda f a) = do
+   env <- askEnv
+   pure $ ValFun env f a
+
+-- Apply Case
+eval (Apply func arg) = do
+  x <- eval func
+  y <- eval arg
+  case (x,y) of
+    (ValFun f_env var b, arg) -> 
+      localEnv (\_ -> envExtend var arg f_env) $ eval b
+    (_,_) -> failure "Cannot apply non-function"
